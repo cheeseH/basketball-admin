5bd11027e3193a09d5185af2ce337c4356fead29
cheeseH <cheese_humn@yahoo.com>
2015-09-13 18:53:43 +0800

    team finnished

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
+var AV = require('../avos');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/testRelation',function(req,res,next){
+	var  query = new AV.Query('Game');
+	query.get('55cd4e0240ac645613921817',{
+		success:function(game){
+			var relation = game.relation('teams');
+			console.log(relation);
+		
+			var teamQuery = relation.query();
+			teamQuery.find({
+				success:function(teams){
+					res.render('gameAdd');
+				},
+				error:function(error){
+
+				}
+			})
+		},
+		error:function(error){
+
+		}
+	})
+})
 
 module.exports = router;
--- routes/team.js
+++ routes/team.js
@@ -5,14 +5,33 @@ var AV = require('../avos.js');
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 var fs = require('fs');
+var qiniu = require('qiniu');
 var TeamController = require('../controller/TeamController');
 var teamController = TeamController();
+var imageUtil = require('../util/image');
 router.post('/testImg',multipartMiddleware,function(req,res,next){
-	var img = req.files.img_file;
-	var path = img.path;
-	var file = new AV.File.withURL('cheese.png',path);
-	file.save();
-	res.render('gameAdd');
+	
+	imageUtil.upLoad(req,res,'imgFile',function(err, ret) {
+                if(!err) {
+                    // 上传成功， 处理返回值
+//                    console.log(ret.key, ret.hash);
+                    res.write(JSON.stringify({
+                        "error" : 0,
+                        "url" : '7xlqng.com1.z0.glb.clouddn.com' + ret.key
+                    }));
+                    console.log("上传成功！");
+                } else {
+                    // 上传失败， 处理返回代码
+//                    console.log(err);
+                    // http://developer.qiniu.com/docs/v6/api/reference/codes.html
+                    res.write(JSON.stringify({
+                        "error" : 1,
+                        "message" : "上传失败"
+                    }));
+                    console.log("上传失败！");
+                }
+                res.end();
+            })
 
 
 });
