b6d500b027118097a33f3f80059264ba3056f536
muzhiqiang <694529694@qq.com>
2015-09-14 21:07:04 +0800

    view

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,46 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get("/login",function(req,res,next){
+	res.render("login");
+})
+router.get("/game",function(req,res,next){
+	res.render('game');
+})
+router.get('/gameInfo',function(req,res,next){
+	res.render('gameInfo');
+})
+router.get('/gameAdd',function(req,res,next){
+	res.render('gameAdd');
+})
+router.get('/team',function(req,res,next){
+	res.render('team');
+})
+router.get('/gameProcess',function(req,res,next){
+	res.render('game-process');
+})
+router.get("/teamAdd",function(req,res,next){
+	res.render('teamAdd');
+})
+router.get("/allTeam",function(req,res,next){
+	res.render("allTeam");
+})
+router.get("/newTeamAdd",function(req,res,next){
+	res.render("newTeamAdd");
+})
+router.get("/oldTeamAdd",function(req,res,next){
+	res.render("oldTeamAdd");
+})
+router.get("/competitionInfo",function(req,res,next){
+	res.render("competitionInfo");
+})
+router.get("/allCompetition",function(req,res,next){
+	res.render("allCompetition");
+})
+router.get("/report",function(req,res,next){
+	res.render("report");
+})
+router.get("/statistics",function(req,res,next){
+	res.render("statistics");
+})
 module.exports = router;
