cf0b886d478f955489734ef24849b4658ace719a
muzhiqiang <694529694@qq.com>
2015-10-17 14:42:03 +0800

    team avoid reclick

--- controller/TeamController.js
+++ controller/TeamController.js
@@ -3,6 +3,7 @@ var Team = AV.Object.extend("Team");
 var Game = AV.Object.extend("Game");
 var Campus = AV.Object.extend('Campus');
 var imageUtil = require('../util/image');
+var 
 function TeamController(){
 }
 
